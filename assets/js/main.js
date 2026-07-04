c3836c4e94f127b32ee5cd263139c953c012f4dd
Miguel <miguel844@gmail.com>
2026-07-04 13:07:06 +0200

    add attempt counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,14 @@ const startBtn = document.getElementById("startBtn");
 const guessInput = document.getElementById("guessInput");
 const checkBtn = document.getElementById("checkBtn");
 const message = document.getElementById("message");
+const history = document.getElementById("history");
+const count = document.getElementById("count");
+
 
 let randomNumber;
+let contador = 0;
+
+
 resetGame();
 
 
@@ -15,13 +21,21 @@ checkBtn.addEventListener("click", function () {
     checkNumber( guessInput.value);
 })
 
+guessInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        checkBtn.click();
+    }
+})
+
 function initGame() {
     guessInput.disabled = false;
     checkBtn.disabled = false;
     startBtn.disabled = true;
     randomNumber = Math.floor(Math.random() * 100) + 1;
-
-    console.log("El numero a adivinar es el " + randomNumber)
+    contador = 0;
+    count.textContent = contador;
+    message.value = "";
+    console.log("El numero a adivinar es el " + randomNumber);
 }
 
 function resetGame() {
@@ -33,9 +47,11 @@ function resetGame() {
 
 function checkNumber(num) {
     num = parseInt(num);
+    contador++;
+    count.textContent = contador;
     // Comprobar que sea un number
     if (num === randomNumber){
-        console.log("Has ganado.");
+        confirm("Has ganado.");
         resetGame();
     } else if(num > randomNumber){
         console.log("El número "+ num +" es mayor.");
